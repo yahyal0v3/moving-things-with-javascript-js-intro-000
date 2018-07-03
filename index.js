93c00e51541b1fcbafa9149013c989e20a3d0674
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-07-03 02:04:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ function moveDodgerLeft() {
 }
 
 function moveDodgerRight() {
-  var rightNumbers = dodger.style.right.replace('px', '')
-  var right = parseInt(rightNumbers, 10)
+  var leftNumbers = dodger.style.left.replace('px', '')
+  var left = parseInt(leftNumbers, 10)
 
-  if (right < 400) {
-    dodger.style.right = `${right + 1}px`
+  if (left < 400) {
+    dodger.style.left = `${right + 1}px`
   }
 }
 
