63acc97b8bd14eea3094f26e9d8c481fe94c5423
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-07-03 02:05:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ function moveDodgerRight() {
   var left = parseInt(leftNumbers, 10)
 
   if (left < 400) {
-    dodger.style.left = `${right + 1}px`
+    dodger.style.left = `${left + 1}px`
   }
 }
 
